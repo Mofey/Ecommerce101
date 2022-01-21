122caca86018a24498056b6afb4edde0d6c3db59
Mofetoluwa Balogun <mofey@outlook.com>
2022-01-22 00:52:14 +0100

    connectDB

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    "BASE_URL": "http://localhost:3000",
+    "MONGODB_URL": "mongodb+srv://Mofetoluwa:Devo$iona1@cluster1.kn8gq.mongodb.net/nextjs-ecommerce?retryWrites=true&w=majority",
+    "ACCESS_TOKEN_SECRET": "4vzu&t-Nej2^U9^_)'[-tUf!c8WZDuud)pL+P7m`yR^&&eL$",
+    "REFRESH_TOKEN_SECRET": "bt9_E7EL6M][Nts#8r<;DQ)VC`c@-m~&-6_j%{:[xy2c~$j>bt4:{'(9`:jZ,8^VXH%Q[:!`X{ctv"
+  }
+}
