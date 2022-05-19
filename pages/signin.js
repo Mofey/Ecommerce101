7aa188fa3b86991a24ce53f4c6381bf01a4513c5
Mofetoluwa Balogun <mofey@outlook.com>
2022-05-19 15:39:55 +0100

    added babelrc

--- pages/signin.js
+++ pages/signin.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+ import Head from 'next/head'
 import Link from 'next/link'
 import {useState, useContext, useEffect} from 'react'
 import {DataContext} from '../store/GlobalState'
