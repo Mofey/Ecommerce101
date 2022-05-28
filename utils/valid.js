072bd8b147157998abd11fa29797b360e7ae0c35
Mofetoluwa Balogun <mofey@outlook.com>
2022-05-28 07:54:34 +0100

    worked on valid file

--- utils/valid.js
+++ utils/valid.js
@@ -1,4 +1,4 @@
-const valid = (name, email, password, cf_password) => {
+export const valid = (name, email, password, cf_password) => {
     if(!name || !email || !password)
     return 'Please add all fields.'
 
@@ -13,9 +13,9 @@ const valid = (name, email, password, cf_password) => {
 }
 
 
-function validateEmail(email) {
+export const validateEmail = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
 
-export default valid
+//export default valid
